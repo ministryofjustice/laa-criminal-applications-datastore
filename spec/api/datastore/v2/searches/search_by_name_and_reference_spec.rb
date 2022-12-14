f2a5884e71f0bce21c9e0a02bbe096824bdcac0a
timpeat <tim.peat@digital.justice.gov.uk>
2022-12-14 10:26:19 +0000

    CRIMRE-83 filter by submitted at

--- spec/api/datastore/v2/searches/search_by_name_and_reference_spec.rb
+++ spec/api/datastore/v2/searches/search_by_name_and_reference_spec.rb
@@ -8,62 +8,60 @@ RSpec.describe 'search with text' do
   let(:search) { { search_text: '' } }
   let(:records) { JSON.parse(response.body).fetch('records') }
 
-  describe 'filter by application_id' do
-    before do
-      details = %i[John Deere 1010 Ken John 1020 Jonathan Deere 1030].each_slice(3)
-      CrimeApplication.insert_all(
-        details.map do |first_name, last_name, reference|
-          {
-            application: {
-              client_details: {
-                applicant: { first_name:, last_name: }
-              },
-              reference: reference
-            }
+  before do
+    details = %i[John Deere 1010 Ken John 1020 Jonathan Deere 1030].each_slice(3)
+    CrimeApplication.insert_all(
+      details.map do |first_name, last_name, reference|
+        {
+          application: {
+            client_details: {
+              applicant: { first_name:, last_name: }
+            },
+            reference: reference
           }
-        end
-      )
-
-      api_request
-    end
-
-    it 'defaults to showing all applications' do
-      expect(records.count).to be 3
-      expect(records.pluck('resource_id')).to match(
-        CrimeApplication.pluck(:id)
-      )
-    end
-
-    context 'when first name is searched' do
-      let(:search) { { search_text: 'John' } }
-
-      it 'shows results that match the first name' do
-        expect(records.pluck('reference')).to match([1010, 1020])
+        }
       end
+    )
+
+    api_request
+  end
+
+  it 'defaults to showing all applications' do
+    expect(records.count).to be 3
+    expect(records.pluck('resource_id')).to match(
+      CrimeApplication.pluck(:id)
+    )
+  end
+
+  context 'when first name is searched' do
+    let(:search) { { search_text: 'John' } }
+
+    it 'shows results that match the first name' do
+      expect(records.pluck('reference')).to match([1010, 1020])
     end
+  end
 
-    context 'when reference is included' do
-      let(:search) { { search_text: 'Deere 1030' } }
+  context 'when reference is included' do
+    let(:search) { { search_text: 'Deere 1030' } }
 
-      it 'shows results that match the reference name' do
-        expect(records.pluck('reference')).to match([1030])
-      end
+    it 'shows results that match the reference name' do
+      expect(records.pluck('reference')).to match([1030])
     end
+  end
 
-    context 'when last_name is searched' do
-      let(:search) { { search_text: 'Deere' } }
+  context 'when last_name is searched' do
+    let(:search) { { search_text: 'Deere' } }
 
-      it 'shows results that match the last name' do
-        expect(records.pluck('reference')).to match([1010, 1030])
-      end
+    it 'shows results that match the last name' do
+      expect(records.pluck('reference')).to match([1010, 1030])
     end
+  end
 
-    context 'when first and last name are searched' do
-      let(:search) { { search_text: 'John Deere' } }
+  context 'when first and last name are searched' do
+    let(:search) { { search_text: 'John Deere' } }
 
-      it 'shows results that match the full name' do
-        expect(records.pluck('reference')).to match([1010])
-      end
+    it 'shows results that match the full name' do
+      expect(records.pluck('reference')).to match([1010])
     end
   end
 end
