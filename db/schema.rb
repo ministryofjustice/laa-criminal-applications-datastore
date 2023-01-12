a12656d1b098b30dd7f9d6b7babe778a62e10de8
timpeat <tim.peat@digital.justice.gov.uk>
2023-01-12 14:36:40 +0000

    Namespace review status and timestamps on crime application.
    Co-authored-by: willmcb <william.mcbrien@digital.justice.gov.uk>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_11_165520) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_12_140053) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,10 +22,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_11_165520) do
     t.datetime "submitted_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }
     t.datetime "returned_at", precision: nil
     t.virtual "searchable_text", type: :tsvector, as: "((to_tsvector('english'::regconfig, (application #>> '{client_details,applicant,first_name}'::text[])) || to_tsvector('english'::regconfig, (application #>> '{client_details,applicant,last_name}'::text[]))) || to_tsvector('english'::regconfig, (application ->> 'reference'::text)))", stored: true
-    t.datetime "completed_at", precision: nil
+    t.datetime "review_completed_at", precision: nil
+    t.string "review_status"
+    t.datetime "review_received_at", precision: nil
+    t.jsonb "return_reason", default: {}
+    t.index ["review_status", "review_completed_at"], name: "index_crime_apps_on_review_status_and_review_completed_at", order: { review_completed_at: :desc }
     t.index ["searchable_text"], name: "index_crime_applications_on_searchable_text", using: :gin
-    t.index ["status", "completed_at"], name: "index_crime_applications_on_status_and_completed_at", order: { completed_at: :desc }
     t.index ["status", "returned_at"], name: "index_crime_applications_on_status_and_returned_at", order: { returned_at: :desc }
+    t.index ["status", "review_completed_at"], name: "index_crime_applications_on_status_and_review_completed_at", order: { review_completed_at: :desc }
     t.index ["status", "submitted_at"], name: "index_crime_applications_on_status_and_submitted_at", order: { submitted_at: :desc }
   end
 
