eecd7a2e3ce5b0d1511e1ada1298320e0aea899e
timpeat <tim.peat@digital.justice.gov.uk>
2023-02-01 12:50:10 +0000

    CRIMRE-87 include more pagination information .
    Include more pagination information from Kaminari to simplify handling
    pagination at the view side.

--- app/api/datastore/entities/pagination.rb
+++ app/api/datastore/entities/pagination.rb
@@ -3,13 +3,16 @@ module Datastore
     class Pagination < Grape::Entity
       present_collection true
 
+      expose :limit_value
       expose :total_pages
       expose :current_page
+      expose :next_page
+      expose :prev_page
       expose :total_count
 
       private
 
-      delegate :total_pages, :current_page, :total_count, to: :collection
+      delegate :limit_value, :total_pages, :current_page, :next_page, :prev_page, :total_count, to: :collection
 
       def collection
         @collection ||= object.fetch(:items)
