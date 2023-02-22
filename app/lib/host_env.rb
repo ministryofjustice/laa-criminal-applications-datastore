728f6d377297b6f3345329b0770eaf034f1fe4e3
Jesus Laiz <zheileman@users.noreply.github.com>
2023-02-22 08:53:47 +0000

    CRIMAP-285 Configure Sentry exceptions reporting (#51)
    Almost identical to the setup we did for Apply.
    
    Added the `HostEnv` class so we can have 1 sentry project handling the exceptions of the different namespaces instead of having to create 1 project per namespace.
    
    So for instance, right now we only have `staging` namespace, exceptions will be reported with `environment` tag set to `staging`.
    Not to be confused with the Rails/Rack env (which is always production no matter the namespace).
    
    For now this will catch any unhandled exception. Later on we can add Sentry reporting for some handled exceptions with additional metadata.
    Example: https://github.com/ministryofjustice/laa-apply-for-criminal-legal-aid/blob/main/app/services/datastore/application_counters.rb#L24

---
+++ app/lib/host_env.rb
@@ -0,0 +1,26 @@
+module HostEnv
+  # Update if more environments are needed
+  NAMED_ENVIRONMENTS = [
+    LOCAL = 'local'.freeze,
+    TEST = 'test'.freeze,
+    STAGING = 'staging'.freeze,
+    PRODUCTION = 'production'.freeze,
+  ].freeze
+
+  class << self
+    NAMED_ENVIRONMENTS.each { |name| delegate "#{name}?", to: :inquiry }
+
+    def env_name
+      return TEST  if Rails.env.test?
+      return LOCAL if Rails.env.development?
+
+      ENV.fetch('ENV_NAME')
+    end
+
+    private
+
+    def inquiry
+      env_name.inquiry
+    end
+  end
+end
