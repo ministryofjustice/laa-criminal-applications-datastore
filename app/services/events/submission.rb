9fc4239b9ea4a5d338922ff51a9d29706c5dbb92
timpeat <tim.peat@digital.justice.gov.uk>
2023-02-27 16:33:30 +0000

    CRIMRE-184 add submitted_at to submission events

--- app/services/events/submission.rb
+++ app/services/events/submission.rb
@@ -3,5 +3,12 @@ module Events
     def name
       'apply.submission'.freeze
     end
+
+    def message
+      {
+        id: crime_application.id,
+        submitted_at: crime_application.submitted_at
+      }
+    end
   end
 end
