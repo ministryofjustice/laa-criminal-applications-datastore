813e05232267d466b1bbdf22c8a8c95901bdf181
timpeat <tim@timpeat.com>
2023-02-28 12:27:16 +0000

    Crimre 186 add parent (#59)
    CRIMRE-186 add parent_id to submission event message

--- spec/services/events/submission_spec.rb
+++ spec/services/events/submission_spec.rb
@@ -2,10 +2,20 @@ require 'rails_helper'
 
 describe Events::Submission do
   let(:crime_application) do
-    instance_double(CrimeApplication, id: 'f7b429cc', submitted_at: DateTime.parse('2023-02-27'))
+    instance_double(
+      CrimeApplication,
+      id: 'f7b429cc',
+      submitted_at: DateTime.parse('2023-02-27'),
+      reference: 673_209,
+      application: { 'parent_id' => '9a123b' }
+    )
   end
 
   it_behaves_like 'an event notification',
                   name: 'apply.submission',
-                  message: { id: 'f7b429cc', submitted_at: DateTime.parse('2023-02-27') }
+                  message: {
+                    id: 'f7b429cc',
+                    submitted_at: DateTime.parse('2023-02-27'),
+                    parent_id: '9a123b'
+                  }
 end
