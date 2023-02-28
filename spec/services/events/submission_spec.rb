514d81e7b5f14a3c28a765edf0797fb65092aaa5
timpeat <tim@timpeat.com>
2023-02-28 09:10:44 +0000

    CRIMRE-184 add submitted_at to submission events (#58)

--- spec/services/events/submission_spec.rb
+++ spec/services/events/submission_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe Events::Submission do
   let(:crime_application) do
-    instance_double(CrimeApplication, id: 'f7b429cc')
+    instance_double(CrimeApplication, id: 'f7b429cc', submitted_at: DateTime.parse('2023-02-27'))
   end
 
   it_behaves_like 'an event notification',
                   name: 'apply.submission',
-                  message: { id: 'f7b429cc' }
+                  message: { id: 'f7b429cc', submitted_at: DateTime.parse('2023-02-27') }
 end
