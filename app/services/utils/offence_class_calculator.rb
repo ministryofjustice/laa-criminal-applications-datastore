cc1c431593bd0f443bb1e9d46456af564dc959c9
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2023-05-11 16:09:09 +0100

    Fix offence class calculation (#93)

--- app/services/utils/offence_class_calculator.rb
+++ app/services/utils/offence_class_calculator.rb
@@ -21,16 +21,16 @@ module Utils
     end
 
     def rank_offences
-      offences_classes = offences.pluck(:offence_class)
+      offences_classes = offences.pluck('offence_class')
       offences_classes.sort_by { |oc| Types::OffenceClass.values.index oc }
     end
 
     def any_manually_entered_offences?
-      offences.any? { |o| o[:offence_class].nil? }
+      offences.any? { |o| o.fetch('offence_class').nil? }
     end
 
     def any_multi_class_offences?
-      offences.any? { |o| o[:offence_class].length > 1 }
+      offences.any? { |o| o.fetch('offence_class').length > 1 }
     end
   end
 end
