2c0dbdbf24fea7e17878b2bdfb85d75573bbd7de
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2023-07-06 14:46:53 +0100

    Use shorthand for signed at field

--- app/api/datastore/entities/v1/maat/application.rb
+++ app/api/datastore/entities/v1/maat/application.rb
@@ -3,16 +3,12 @@ module Datastore
     module V1
       module MAAT
         class Application < BaseApplicationEntity
-          expose :declaration_signed_at
+          expose :submitted_at, as: :declaration_signed_at
 
           private
 
           def case_details
             super.except('offences', 'codefendants')
-          end
-
-          def declaration_signed_at
-            submitted_value('submitted_at')
           end
         end
       end
