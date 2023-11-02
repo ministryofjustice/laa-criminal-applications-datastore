33af242712c59b742ab9ebbbc5bb37e3f639708a
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2023-11-02 09:35:30 +0000

    CRIMAP-728 Add basic replication

--- app/models/crime_application.rb
+++ app/models/crime_application.rb
@@ -1,3 +1,5 @@
+require 'laa_crime_schemas'
+
 class CrimeApplication < ApplicationRecord
   include Redactable
 
@@ -7,6 +9,7 @@ class CrimeApplication < ApplicationRecord
 
   before_validation :shift_payload_attributes, on: :create
   before_validation :set_overall_offence_class, on: :create
+  before_validation :copy_first_court_name, on: :save
 
   private
 
@@ -24,5 +27,14 @@ class CrimeApplication < ApplicationRecord
     self.offence_class = Utils::OffenceClassCalculator.new(
       offences: submitted_application['case_details']['offences']
     ).offence_class
+  end
+
+  # Replicate the 'hearing_court_name' into 'first_court_hearing_name' even though they
+  # are assumed to be the same when `is_first_court_hearing = yes`
+  def copy_first_court_name
+    return if first_court_name.present?
+    return if is_first_court_hearing != LaaCrimeSchemas::Types::FirstHearingAnswerValues['yes']
+
+    self.first_court_name = hearing_court_name
   end
 end
