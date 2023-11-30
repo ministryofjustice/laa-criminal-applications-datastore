242515a46ae622c77d6473bc78ea6eee4d2af7f5
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2023-11-30 17:11:35 +0000

    CRIMAPP-232 add work stream to submission event message (#188)
    Co-authored-by: timpeat <tim@timpeat.com>

--- app/services/events/submission.rb
+++ app/services/events/submission.rb
@@ -8,7 +8,8 @@ module Events
       {
         id: crime_application.id,
         submitted_at: crime_application.submitted_at,
-        parent_id: crime_application.submitted_application['parent_id']
+        parent_id: crime_application.submitted_application['parent_id'],
+        work_stream: crime_application.work_stream
       }
     end
   end
