cc38f4fb2601f445317c625e2fe1dc74806280bc
timpeat <tim@timpeat.com>
2024-02-01 15:59:06 +0000

    Ensure maat application only inlcude attributes in the schema
    Test will only pass if new attributes add to the MAAT entity
    are also added to the maat_schema

--- spec/api/datastore/v1/maat/applications_spec.rb
+++ spec/api/datastore/v1/maat/applications_spec.rb
@@ -6,11 +6,14 @@ RSpec.describe 'get application ready for maat' do
 
     let(:application) do
       CrimeApplication.create(
-        submitted_application: JSON.parse(LaaCrimeSchemas.fixture(1.0).read),
+        submitted_application: submitted_application,
         review_status: :ready_for_assessment
       )
     end
 
+    let(:submitted_application) do
+      JSON.parse(LaaCrimeSchemas.fixture(1.0).read)
+    end
     let(:application_usn) { application.submitted_application['reference'] }
     let(:maat_application) { JSON.parse(response.body) }
 
@@ -20,53 +23,7 @@ RSpec.describe 'get application ready for maat' do
 
     context 'with a ready for assessment application' do
       before do
-        allow(Utils::OffenceClassCalculator).to receive(:new) {
-                                                  instance_double(Utils::OffenceClassCalculator,
-                                                                  offence_class: calculated_offence_class)
-                                                }
         api_request
-      end
-
-      let(:calculated_offence_class) { Types::OffenceClass.values.sample }
-
-      # rubocop:disable Layout/LineLength
-      let(:expected_case_details) do
-        {
-          'case_type' => application.submitted_application['case_details']['case_type'],
-          'appeal_maat_id' => application.submitted_application['case_details']['appeal_maat_id'],
-          'appeal_lodged_date' => application.submitted_application['case_details']['appeal_lodged_date'],
-          'appeal_with_changes_details' => application.submitted_application['case_details']['appeal_with_changes_details'],
-          'hearing_court_name' => application.submitted_application['case_details']['hearing_court_name'],
-          'hearing_date' => application.submitted_application['case_details']['hearing_date'],
-          'offence_class' => calculated_offence_class,
-          'urn' => application.submitted_application['case_details']['urn'],
-          'date_case_concluded' => application.submitted_application['case_details']['date_case_concluded'],
-          'has_case_concluded' => application.submitted_application['case_details']['has_case_concluded'],
-          'is_client_remanded' => application.submitted_application['case_details']['is_client_remanded'],
-          'date_client_remanded' => application.submitted_application['case_details']['date_client_remanded'],
-          'is_preorder_work_claimed' => application.submitted_application['case_details']['is_preorder_work_claimed'],
-          'preorder_work_date' => application.submitted_application['case_details']['preorder_work_date'],
-          'preorder_work_details' => application.submitted_application['case_details']['preorder_work_details']
-        }
-      end
-      # rubocop:enable Layout/LineLength
-
-      let(:expected_maat_application) do
-        {
-          'application_type' => application.submitted_application['application_type'],
-          'reference' => application.submitted_application['reference'],
-          'reviewed_at' => application.reviewed_at,
-          'client_details' => application.submitted_application['client_details'],
-          'provider_details' => application.submitted_application['provider_details'],
-          'submitted_at' => application.submitted_application['submitted_at'],
-          'declaration_signed_at' => application.submitted_application['submitted_at'],
-          'date_stamp' => application.submitted_application['date_stamp'],
-          'means_passport' => application.submitted_application['means_passport'],
-          'ioj_bypass' => application.submitted_application['interests_of_justice'].empty?,
-          'case_details' => expected_case_details,
-          'schema_version' => application.submitted_application['schema_version'],
-          'id' => application.id
-        }
       end
 
       it 'returns http status 200' do
@@ -78,15 +35,29 @@ RSpec.describe 'get application ready for maat' do
           LaaCrimeSchemas::Validator.new(response.body, version: 1.0, schema_name: 'maat_application')
         ).to be_valid
       end
+    end
 
-      it 'returns the required application details for maat integration' do
-        expect(maat_application).to match(expected_maat_application)
+    describe 'returning the calcuated offence class' do
+      let(:offence_class) { Types::OffenceClass.values.sample }
+
+      before do
+        allow(Utils::OffenceClassCalculator).to receive(:new).and_return(
+          instance_double(Utils::OffenceClassCalculator, offence_class:)
+        )
+        api_request
+      end
+
+      it 'is included in the case_details' do
+        expect(maat_application['case_details']['offence_class']).to eq(offence_class)
       end
     end
 
     context 'with a completed application' do
       before do
-        application.update!(review_status: :assessment_completed, reviewed_at: 1.week.ago)
+        application.update!(
+          review_status: :assessment_completed,
+          reviewed_at: DateTime.new(2022, 12, 1, 12, 28, 58.001)
+        )
         api_request
       end
 
