24d6d18d564a5a44afeaa44a7b42e9791c65a29a
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2024-05-22 15:47:17 +0100

    Fix linting errors

--- spec/services/redacting/redact_spec.rb
+++ spec/services/redacting/redact_spec.rb
@@ -58,10 +58,10 @@ describe Redacting::Redact do
           'relationship_to_partner' => 'living_together',
           'separation_date' => nil,
           'benefit_check_result' => false,
-          'confirm_details' => "yes",
-          'confirm_dwp_result' => "no",
-          'has_benefit_evidence' => "no",
-          'has_nino' => "yes",
+          'confirm_details' => 'yes',
+          'confirm_dwp_result' => 'no',
+          'has_benefit_evidence' => 'no',
+          'has_nino' => 'yes',
           'will_enter_nino' => nil,
         })
       end
