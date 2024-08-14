4c33d11c2c53f92df56006a82c738de017fcfa16
Mo Seedat <47113046+mseedat-moj@users.noreply.github.com>
2024-08-14 10:08:02 +0100

    [CRIMAPP-1258] Allow cifc reason exposure (#307)
    * CRIMAPP-1258 Allow cifc reason exposure
    
    * Update schema

--- app/api/datastore/entities/v1/change_in_financial_circumstances_application.rb
+++ app/api/datastore/entities/v1/change_in_financial_circumstances_application.rb
@@ -5,6 +5,7 @@ module Datastore
         expose :pre_cifc_reference_number
         expose :pre_cifc_maat_id
         expose :pre_cifc_usn
+        expose :pre_cifc_reason
       end
     end
   end
