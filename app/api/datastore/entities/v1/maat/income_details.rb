f7b98c875adbce9209f85baaeb9ce6a56a4af861
Ripan <er.ripankumar@gmail.com>
2024-08-23 19:16:11 +0100

    Update income_details.rb

--- app/api/datastore/entities/v1/maat/income_details.rb
+++ app/api/datastore/entities/v1/maat/income_details.rb
@@ -17,13 +17,13 @@ module Datastore
 
           def income_payments
             Utils::MAAT::OtherIncomePaymentCalculator.new(
-              payments: object['income_payments'].map(&:dup),
+              payments: object['income_payments'].map(&:deep_dup),
             ).call
           end
 
           def income_benefits
             Utils::MAAT::OtherIncomeBenefitCalculator.new(
-              payments: object['income_benefits'].map(&:dup),
+              payments: object['income_benefits'].map(&:deep_dup),
             ).call
           end
         end
