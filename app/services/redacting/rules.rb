1c8c03c56ea043a1d2cc577b0f917d495b646110
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2025-04-16 10:03:16 +0100

    new attributes to redact

--- app/services/redacting/rules.rb
+++ app/services/redacting/rules.rb
@@ -7,7 +7,7 @@ module Redacting
         redact: %w[legal_rep_telephone]
       },
       'client_details.applicant' => {
-        redact: %w[first_name last_name other_names nino arc telephone_number]
+        redact: %w[first_name last_name other_names nino arc telephone_number date_of_birth]
       },
       'client_details.applicant.home_address' => {
         redact: %w[lookup_id address_line_one address_line_two]
@@ -15,17 +15,25 @@ module Redacting
       'client_details.applicant.correspondence_address' => {
         redact: %w[lookup_id address_line_one address_line_two]
       },
+      'client_details.partner' => {
+        redact: %w[first_name last_name other_names date_of_birth nino],
+        type: :object
+      },
       'case_details.codefendants' => {
         redact: %w[first_name last_name],
-        type: :array # [{}, {}, ...]
+        type: :array
+      },
+      'case_details' => {
+        redact: %w[urn hearing_court_name hearing_date],
+        type: :object
       },
       'interests_of_justice' => {
         redact: %w[reason],
-        type: :array # [{}, {}, ...]
+        type: :array
       },
       'supporting_evidence' => {
         redact: %w[s3_object_key filename],
-        type: :array # [{}, {}, ...]
+        type: :array
       },
       'additional_information' => {
         redact: :value,
@@ -34,6 +42,10 @@ module Redacting
       'date_stamp_context' => {
         redact: %w[first_name last_name]
       },
+      'means_details' => {
+        redact: %w[income_details.income_payments.amount income_details.income_benefits.amount
+                   income_details.employment_type income_details.partner_employment_type capital_details.premium_bonds_total_value capital_details.partner_premium_bonds_total_value capital_details.trust_fund_amount_held capital_details.trust_fund_yearly_dividend capital_details.partner_trust_fund_amount_held capital_details.partner_trust_fund_yearly_dividend outgoings_details.outgoings.amount]
+      }
     }.freeze
 
     def self.pii_attributes
--- spec/services/redacting/rules_spec.rb
+++ spec/services/redacting/rules_spec.rb
@@ -12,11 +12,15 @@ describe Redacting::Rules do
           client_details.applicant
           client_details.applicant.correspondence_address
           client_details.applicant.home_address
+          client_details.applicant.date_of_birth
+          client_details.partner
           case_details.codefendants
+          case_details
           interests_of_justice
           supporting_evidence
           additional_information
           date_stamp_context
+          means_details
         ]
       )
     end
