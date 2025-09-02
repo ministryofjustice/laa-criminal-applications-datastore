5d1bfbd198c14df40e2a8fa5393ca6c7b4001bbc
timpeat <tim@timpeat.com>
2025-09-02 16:09:35 +0100

    Add rails event store

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_28_142537) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_02_160858) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -68,6 +68,30 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_28_142537) do
     t.index ["crime_application_id"], name: "index_decisions_on_crime_application_id"
   end
 
+  create_table "event_store_events", force: :cascade do |t|
+    t.uuid "event_id", null: false
+    t.string "event_type", null: false
+    t.jsonb "metadata"
+    t.jsonb "data", null: false
+    t.datetime "created_at", null: false
+    t.datetime "valid_at"
+    t.index ["created_at"], name: "index_event_store_events_on_created_at"
+    t.index ["event_id"], name: "index_event_store_events_on_event_id", unique: true
+    t.index ["event_type"], name: "index_event_store_events_on_event_type"
+    t.index ["valid_at"], name: "index_event_store_events_on_valid_at"
+  end
+
+  create_table "event_store_events_in_streams", force: :cascade do |t|
+    t.string "stream", null: false
+    t.integer "position"
+    t.uuid "event_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["created_at"], name: "index_event_store_events_in_streams_on_created_at"
+    t.index ["event_id"], name: "index_event_store_events_in_streams_on_event_id"
+    t.index ["stream", "event_id"], name: "index_event_store_events_in_streams_on_stream_and_event_id", unique: true
+    t.index ["stream", "position"], name: "index_event_store_events_in_streams_on_stream_and_position", unique: true
+  end
+
   create_table "redacted_crime_applications", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "crime_application_id"
     t.jsonb "submitted_application", default: {}, null: false
@@ -78,5 +102,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_28_142537) do
   end
 
   add_foreign_key "decisions", "crime_applications"
+  add_foreign_key "event_store_events_in_streams", "event_store_events", column: "event_id", primary_key: "event_id"
   add_foreign_key "redacted_crime_applications", "crime_applications"
 end
