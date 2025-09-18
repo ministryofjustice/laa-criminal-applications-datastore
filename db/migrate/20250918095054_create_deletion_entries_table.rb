9374c96215cc301de408a8815e6ff551ffa07efd
Edwin Kruglov <edwin.kruglov@justice.gov.uk>
2025-09-18 15:46:12 +0100

    [CRIMAPP-1944] Create deletion_entries (#417)

---
+++ db/migrate/20250918095054_create_deletion_entries_table.rb
@@ -0,0 +1,14 @@
+class CreateDeletionEntriesTable < ActiveRecord::Migration[7.2]
+  def change
+    create_table :deletion_entries_tables, id: :uuid do |t|
+      t.string :record_id, null: false
+      t.string :record_type, null: false
+      t.string :business_reference
+      t.string :deleted_by, null: false
+      t.string :deleted_from
+      t.string :reason, null: false
+
+      t.timestamps
+    end
+  end
+end
