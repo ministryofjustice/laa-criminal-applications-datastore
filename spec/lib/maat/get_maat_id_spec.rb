4c66513e9e88e999a32dd164d49e977363995b96
Hibo <51047911+hiboabd@users.noreply.github.com>
2025-10-02 10:59:26 +0100

    [CRIMAPP-1903] Add service to get maat id (#420)
    * Add service to get maat id
    
    * Fix failing spec
    
    * Refactor

---
+++ spec/lib/maat/get_maat_id_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+describe MAAT::GetMAATId do
+  subject(:get_maat_id) { described_class.new(http_client: http_client.call) }
+
+  let(:http_client) { instance_double(MAAT::HttpClient, call: connection) }
+  let(:response) { instance_double(Faraday::Response, body:) }
+  let(:connection) { instance_double(Faraday::Connection) }
+  let(:usn) { 123_456 }
+  let(:maat_id) { 60_123 }
+
+  let(:body) { { 'maat_ref' => 60_123 } }
+
+  before do
+    allow(connection).to receive(:get) { response }
+  end
+
+  describe '#by_usn!' do
+    subject(:get_by_usn!) { get_maat_id.by_usn!(usn) }
+
+    it 'makes a GET request to the MAAT API USN path' do
+      get_by_usn!
+
+      expect(connection).to have_received(:get)
+        .with('/api/external/v1/crime-application/result/usn/123456')
+    end
+
+    context 'when decision is empty found' do
+      let(:body) { {} }
+
+      it 'returns nil' do
+        expect(get_by_usn!).to be_nil
+      end
+    end
+
+    context 'when decision found has no maat id' do
+      let(:body) { { 'maat_ref' => nil } }
+
+      it 'returns nil' do
+        expect(get_by_usn!).to be_nil
+      end
+    end
+  end
+end
