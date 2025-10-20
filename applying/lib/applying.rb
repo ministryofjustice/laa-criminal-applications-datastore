efe24041b8962ec82e87c2447847b44113246f8a
EdwinKruglov <edwin.kruglov@justice.gov.uk>
2025-10-20 16:19:23 +0100

    Rcord submitted event

--- applying/lib/applying.rb
+++ applying/lib/applying.rb
@@ -1,5 +1,16 @@
 module Applying
-  class Event < RailsEventStore::Event; end
+  class Event < RailsEventStore::Event
+    def self.from_application(crime_application)
+      new(
+        data: {
+          entity_id: crime_application.id,
+          entity_type: crime_application.application_type,
+          business_reference: crime_application.reference
+        }
+      )
+    end
+  end
+
   class DraftCreated < Event; end
   class DraftUpdated < Event; end
   class DraftDeleted < Event; end
