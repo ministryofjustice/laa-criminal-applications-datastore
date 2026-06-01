26c27c5fb0e6c6298296c72da47af2c40e395c0f
hiboabd <51047911+hiboabd@users.noreply.github.com>
2026-06-01 14:39:56 +0100

    Record deletion log count via background thread

--- app/lib/prometheus_metrics/deletion_log_reporter.rb
+++ app/lib/prometheus_metrics/deletion_log_reporter.rb
@@ -1,5 +1,7 @@
 module PrometheusMetrics
   class DeletionLogReporter
+    REPORT_INTERVAL = ENV.fetch('DELETION_LOG_REPORT_INTERVAL', 60).to_i # seconds
+
     def self.report(count)
       client = PrometheusExporter::Client.default
 
@@ -10,5 +12,21 @@ module PrometheusMetrics
     rescue StandardError => e
       Rails.logger.warn("Failed to report deletion log metric: #{e.message}")
     end
+
+    # :nocov:
+    def self.start
+      Rails.logger.info "[DeletionLogReporter] Starting background reporter (interval: #{REPORT_INTERVAL}s)"
+
+      Thread.new do
+        loop do
+          report(DeletionEntry.count)
+          sleep REPORT_INTERVAL
+        rescue StandardError => e
+          Rails.logger.warn("DeletionLogReporter error: #{e.message}")
+          sleep REPORT_INTERVAL
+        end
+      end
+    end
+    # :nocov:
   end
 end
